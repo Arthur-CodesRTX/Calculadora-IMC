67893916417be4c90e4cb06f48ba7a16499982ad
Arthur <poeta.codes24@gmail.com>
2026-01-31 23:42:18 -0300

    style: Atuliazação do title e do texto refatorado.

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -1,17 +1,20 @@
-function HeroText(){
-
-    return (
-        <section className="text-left flex flex-col gap-12 my-12">
-            <h1 className="text-5xl font-bold">
-                <span className="drop-shadow-[0_3px_6px_rgba(0,0,0,0.5)]">
-                    <span className="text-5xl bg-linear-to-b from-[#F6C90E] to-[#F7E083] bg-clip-text text-transparent font-medium">Transforme dados em resultados</span>
-                </span>
-            </h1>
-            <p className="text-[#F7F7F7] text-3xl w-4/10 [text-shadow:1px_2px_5px_rgba(0,0,0,0.5)] font-normal leading-snug">Descubra seu IMC e gasto calórico Total em segundos. Nossa calculadora inteligente analisa seu perfil físico e nível de atividade <br /><b>Simples, preciso e visual.</b></p>
-            <p className="text-[#F7F7F7] text-3xl w-4/10 [text-shadow:1px_2px_5px_rgba(0,0,0,0.5)] font-normal leading-snug">Descubra seu IMC e gasto calórico Total em segundos. Nossa calculadora inteligente analisa seu perfil físico e nível de atividade <br /><b>Simples, preciso e visual.</b></p>
-        </section>
-    )
-
+function HeroText() {
+  return (
+    <section className="text-left flex flex-col gap-12 my-12">
+      <h1 className="text-5xl font-bold">
+        <span className="drop-shadow-[0_3px_6px_rgba(0,0,0,0.5)]">
+          <span className="text-5xl bg-linear-to-b from-[#F6C90E] to-[#F7E083] bg-clip-text text-transparent font-medium">
+            Transforme dados em resultados
+          </span>
+        </span>
+      </h1>
+      <p className="text-[#F7F7F7] text-3xl w-4/10 [text-shadow:1px_2px_5px_rgba(0,0,0,0.5)] font-normal leading-snug">
+        Descubra seu IMC e gasto calórico Total em segundos. Nossa calculadora
+        inteligente analisa seu perfil físico e nível de atividade. <br />
+        <b>Simples, preciso e visual.</b>
+      </p>
+    </section>
+  );
 }
 
 export default HeroText;
